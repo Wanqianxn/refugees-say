f8e94a353c74375a6d25d2b20b78105fbec8f20f
wanqianxn <ywanqian95@gmail.com>
2017-10-29 22:09:00 -0400

    backend

--- application.py
+++ application.py
@@ -1,25 +1,37 @@
-
 from flask import Flask, redirect, render_template, request, url_for, send_file, session
 import os
 import pandas as pd
+import numpy as np
 import helpers
+
 
 app = Flask(__name__)
 
+sites = pd.read_csv('data/sites.csv')
+cases = pd.read_csv('data/cases.csv')
+output = pd.read_csv('data/output.csv')
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+  # Assign by output.csv:
+  csvAssignment = helpers.CSVAlgo(cases, sites, output)
+  DictbySite = dict()
+  for city in sites['city']:
+    key = (city, sites[sites['city'] == city].iloc[0]['state'])
+    DictbySite[key] = csvAssignment.dictOfRefugees(city)
+  csvCommInfo = csvAssignment.dictOfCommunities()
 
-@app.route("/map")
-def map():
-	sites = pd.read_csv('data/sites.csv')
-	cases = pd.read_csv('data/cases.csv')
-	output = pd.read_csv('data/output.csv')
+  # Assign randomly:
+  randomAssignment = helpers.RandomAlgo(cases, sites)
+  for city in sites['city']:
+    randomAssignment.dictOfRefugees(city)
+  randAssignmentsByCommunity = randomAssignment.randDictbySite
+  randCommInfo = randomAssignment.dictOfCommunities()
 
-  # Call helpers functions refugeesInfo and getFromCommunity here
+  # For CSV assignment: communities=DictbySite, comminfo=csvCommInfo
+  # For random assignment: communities=randAssignmentsByCommunity, comminfo=randCommInfo
+  return render_template('index.html', communities=randAssignmentsByCommunity, comminfo=randCommInfo)
 
-	# pass two dataframes to front end
-	return render_template("map.html", sites=sites.to_html(), cases=cases.to_html())
 
 # Run application
 if __name__ == "__main__":
